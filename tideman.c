6032af141a44730f3e2bafa49c3df7c782cb9c2e
fiker18 <fikerketema@icloud.com>
2025-10-19 23:20:26 +0300

    Update tideman.c

--- tideman.c
+++ tideman.c
@@ -147,21 +147,38 @@ void record_preferences(int ranks[])
 }
 
 // Record pairs of candidates where one is preferred over the other
-void add_pairs(void)
+void lock_pairs(void)
 {
-    for(int i = 0; i < candidate_count; i++)
+    for(int i = 0; i < pair_count; i++)
     {
-        for(int j = 0; j < candidate_count; j++)
+        if (!check_cycle(pairs[i].winner, pairs[i].loser))
         {
-            if(preferences[i][j] > preferences[j][i])
-            {
-                pairs[pair_count].winner = i;
-                pairs[pair_count].loser = j;
-                pair_count++;
-            }
+            locked[pairs[i].winner][ pairs[i].loser] = true;
         }
     }
     return;
+}
+
+bool check_cycle(int winner, int loser)
+{
+    if(locked[loser][winner])
+    {
+        return true;
+    }
+    else
+    {
+        for(int i = 0; i < candidate_count; i++)
+        {
+            if(locked[loser][i])
+            {
+                if(check_cycle(winner,i))
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
 
 // Sort pairs in decreasing order by strength of victory
