b489a2eb438bb331921062b32cee3e7b124523ab
Fiker Aklile <fikeraklile@Fikers-MacBook-Air.local>
2025-11-30 23:31:11 +0300

    version 2

--- Final/app.py
+++ Final/app.py
@@ -1,5 +1,6 @@
 import os
 
+from werkzeug.utils import secure_filename
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
@@ -9,9 +10,12 @@ from functools import wraps
 # Configure application
 app = Flask(__name__)
 
+UPLOAD_FOLDER = "static/uploads"
 
 
 # Configure session to use filesystem (instead of signed cookies)
+
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
@@ -19,7 +23,7 @@ Session(app)
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///finalproject.db")
 
-libraries = ["ALL", "OWNFOODS", "FAVOURITES"]
+
 
 def login_required(f):
     """
@@ -47,52 +51,45 @@ def after_request(response):
 
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 @login_required
 def index():
-    libraries = ["ALL", "OWNFOODS", "FAVOURITES"]
+    libraries = []
 
-    if request.method == "GET":
-        list_of_foods = db.execute("SELECT * FROM ownFood ORDER BY title ASC")
-        return render_template("homepage.html", list_of_foods=list_of_foods, libraries=libraries)
+    lib = db.execute("SELECT library FROM library WHERE user_id = (?) GROUP BY library", session["user_id"])
+    for dic in lib:
+        libraries.append(dic["library"])
+
+    selected_library = request.args.get("library")
+    search = request.args.get("search")
+
+    if selected_library:
+        list_of_foods = db.execute("SELECT * FROM ownFood WHERE foodid IN (SELECT food_id FROM library WHERE user_id = (?) AND library = (?)) ORDER BY TITLE", session["user_id"], selected_library)
+
+        if not list_of_foods or selected_library == "ALL":
+            list_of_foods = db.execute("SELECT * FROM ownFood ORDER BY TITLE")
+    elif search:
+        list_of_foods = db.execute("SELECT * FROM ownFood WHERE TITLE LIKE (?) ORDER BY TITLE", search)
+        selected_library = "ALL"
     else:
-        selected_library = request.form.get("library")
-        search = request.form.get("search")
-        order = request.form.get("order")
+        list_of_foods = db.execute("SELECT * FROM ownFood ORDER BY TITLE")
+        selected_library = "ALL"
 
-        if order:
-            if not selected_library and search:
-                list_of_foods = db.execute("SELECT * FROM ownFood WHERE title LIKE (% ? %) ORDER BY (?) DESC", search)
-            if selected_library and not search:
-                list_of_foods = db.execute("SELECT * FROM ownFood WHERE id IN (SELECT * FROM {selected_library}) ORDER BY (?) DESC", order)
-            if selected_library and search:
-                list_of_foods = db.execute("SELECT * FROM ownFood WHERE id IN (SELECT * FROM {selected_library}) AND title LIKE (% ? %) ORDER BY (?) DESC", search, order)
-        else:
-            if not selected_library and search:
-                list_of_foods = db.execute("SELECT * FROM ownFood WHERE title LIKE (% ? %) ORDER BY title ASC", search)
-            if selected_library and not search:
-                list_of_foods = db.execute("SELECT * FROM ownFood WHERE id IN (SELECT * FROM {selected_library}) AND user_id = (?) ORDER BY title ASC")
-            if selected_library and search:
-                list_of_foods = db.execute("SELECT * FROM ownFood WHERE id IN (SELECT * FROM {selected_library}) AND title LIKE (% ? %) ORDER BY title ASC", search)
-
-        return render_template("homepage.html", list_of_foods=list_of_foods, libraries=libraries)
-
-@app.route("/addlibrary")
-@login_required
-def addlibrary():
-
-    table = request.form.get("table")
-    food = request.form.get("food_id")
-
-    db.execute = ("INSERT INTO libraries (user_id, library, food_id) VALUES ( ?, ?, ?)", session["user_id"], table, food)
-
-    library.append(table)
-    return redirect("/")
+    return render_template("homepage.html", list_of_foods=list_of_foods, libraries=libraries, selected_library=selected_library)
 
 
-@app.route("/newlibrary")
+
+@app.route("/set_library", methods=["POST"])
 @login_required
 def newlibrary():
+    library = request.form.get("library")
+    new_library = request.form.get("new_library")
+    food_id = request.form.get("food_id")
+
+    if (library) and (food_id):
+        db.execute("INSERT INTO library (user_id, food_id, library) VALUES ( ?, ?, ?)", session["user_id"], food_id, library)
+    elif (new_library) and (food_id):
+        db.execute("INSERT INTO library (user_id, food_id, library) VALUES ( ?, ?, ?)", session["user_id"], food_id, new_library)
     return redirect("/")
 
 
@@ -152,9 +149,64 @@ def login():
         if len(user) == 1 and check_password_hash(user[0]["hash"], password):
             session["user_id"] = user[0]["id"]
             return redirect("/")
+        else:
+            flash("Username or Password Incorrect")
+            return redirect("/login")
 
 
 @app.route("/food")
 @login_required
 def food():
-    return render_template("food.html")
+    libraries = []
+
+    lib = db.execute("SELECT library FROM library WHERE user_id = (?) GROUP BY library", session["user_id"])
+    for dic in lib:
+        libraries.append(dic["library"])
+
+    food_id = request.args.get("food_id")
+    if not food_id:
+        return render_template("food.html")
+    else:
+        food = db.execute("SELECT * FROM ownFood WHERE foodid = (?)", food_id)
+        return render_template("food.html", food=food[0], libraries=libraries)
+
+
+@app.route("/foodcreate", methods=["GET", "POST"])
+@login_required
+def save_food():
+    if request.method == "GET":
+         return render_template("foodcreate.html")
+    else:
+        title = request.form.get("title")
+        description = request.form.get("description")
+        ingredients = request.form.get("ingredients")
+        procedure = request.form.get("procedure")
+        image = request.files.get("image")
+        duration = request.form.get("duration")
+
+        # Validate fields
+        if not (title and description and ingredients and procedure):
+            flash("All fields are required")
+            return redirect("/foodcreate")
+
+        # Handle image upload
+        image_filename = None
+        if image and image.filename != "":
+            image_filename = secure_filename(image.filename)
+            image.save(os.path.join(app.config["UPLOAD_FOLDER"], image_filename))
+
+        # Insert into database
+        db.execute(
+            """
+            INSERT INTO ownFood (TITLE, DESCRIPTION, RECIPE, PROCEDURE, DURATIONS, IMAGE, user_id)
+            VALUES (?, ?, ?, ?, ?, ?, ?)
+            """,
+            title,
+            description,
+            ingredients,
+            procedure,
+            duration,
+            image_filename,
+            session["user_id"]
+        )
+        return redirect("/")
